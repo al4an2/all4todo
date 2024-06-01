c634bf3173a72470d8f7be17f73033ae315efb6e
al4an2 <al4an2@gmail.com>
2024-06-01 16:20:24 +0200

    added annotations and docstrings

--- app/app.py
+++ app/app.py
@@ -1,12 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+db = SQLAlchemy(app)
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
 
 
 @app.route('/')
 def index():
-    return "Hello,World!"
+    tasks = Task.query.all()
+    return render_template('index.html', tasks=tasks)
+
+
+@app.route('/add', methods=['POST'])
+def add_task():
+    task_content = request.form['content']
+    new_task = Task(content=task_content)
+    db.session.add(new_task)
+    db.session.commit()
+    return redirect('/')
 
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
